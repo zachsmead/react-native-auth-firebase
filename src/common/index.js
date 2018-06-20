c4adcfb9e4a0cd9f410b1f7f334cc6510428d8a7
= <zrsmead@gmail.com>
2018-06-20 14:52:47 -0700

    components and common folder

---
+++ src/common/index.js
@@ -0,0 +1,8 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+
+// The above pattern both imports and exports components.
+// When using this pattern, you can't use the 'default'
+// keyword in the files you're importing.
