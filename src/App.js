ada9ca93d8abff4c03715cd3a9529ef1ba1e8951
= <zrsmead@gmail.com>
2018-06-20 20:06:42 -0700

    adding cmoponents for login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCDeADETnWAMNTeYkzdmBzWy3G8I9L8irI',
+      authDomain: 'authentication-fdddb.firebaseapp.com',
+      databaseURL: 'https://authentication-fdddb.firebaseio.com',
+      projectId: 'authentication-fdddb',
+      storageBucket: 'authentication-fdddb.appspot.com',
+      messagingSenderId: '143323193780'
+    });
+  }
+
   render() {
     return (
       <View>
-        <Text>ASDF</Text>
+        <Header headerText="Authentication" />
+        // <LoginForm />
       </View>
     );
   }
