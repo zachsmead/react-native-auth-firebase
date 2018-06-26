6a4bd9edc86cd5f231ff92e6c4709d5a1f33ae43
= <zrsmead@gmail.com>
2018-06-26 14:05:38 -0700

    styling on button

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -25,6 +25,7 @@ const styles = {
     alignSelf: 'stretch',
     backgroundColor: '#fff',
     borderRadius: 5,
+    borderWidth: 1,
     borderColor: '#007aff',
     marginLeft: 5,
     marginRight: 5
